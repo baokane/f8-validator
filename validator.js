aacea6709fa9f264d6be9791c0b8dea01ba65261
tran khanh bao <tranbao22051999@gmail.com>
2023-06-09 16:11:32 +0700

    file

--- validator.js
+++ validator.js
@@ -1,11 +1,34 @@
 function Validator(option) {
+
+
+    function validate(inputElement, rule) {
+        var errorMessage = rule.test(inputElement.value)
+        var errorElement = inputElement.parentElement.querySelector(option.errorSelector)
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add('invalid')
+        } else {
+            errorElement.innerText = '';
+            inputElement.parentElement.classList.remove('invalid')
+        }
+    }
+
     var formElement = document.querySelector(option.form)
+
     if (formElement) {
         option.rules.forEach(function (rule) {
             var inputElement = formElement.querySelector(rule.selector);
+
             if (inputElement) {
                 inputElement.onblur = function () {
-                    console.log(rule.selector)
+                    //rule.test: hàm , inputElement.value: giá trị input người dùng nhập vào
+                    validate(inputElement, rule);
+                }
+                inputElement.oninput = function () {
+                    var errorElement = inputElement.parentElement.querySelector(option.errorSelector)
+                    errorElement.innerText = '';
+                    inputElement.parentElement.classList.remove('invalid')
                 }
             }
         })
@@ -15,8 +38,8 @@ function Validator(option) {
 Validator.isRequired = function (selector) {
     return {
         selector: selector,
-        test: function () {
-
+        test: function (value) {
+            return value.trim() ? undefined : 'vui lòng nhập trường này'
         }
     }
 }
@@ -24,8 +47,12 @@ Validator.isRequired = function (selector) {
 Validator.isEmail = function (selector) {
     return {
         selector: selector,
-        test: function () {
-
+        test: function (value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+            return regex.test(value) ? undefined : 'vui lòng nhập email'
         }
     }
 }
+
+//isrequired , isEmail, formElement, inputElement, errorMessage, errorElement, value: của test
+
